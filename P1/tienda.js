88289479fddfb01c438e0b752d2817784241ca82
AleGran <a.grandeg.2019@alumnos.urjc.es>
2025-07-02 00:05:02 +0200

    Cambio entre páginas

--- P1/tienda.js
+++ P1/tienda.js
@@ -8,6 +8,14 @@ const PUERTO = 8080;
 const index_path = './index.html';
 const error_path = './error.html';
 
+//-- Extensiones de los posibles recursos
+const mimeTypes = {
+    '.html': { type: 'text/html' },
+    '.css': { type: 'text/css' },
+    '.ico': { type: 'image/x-icon' },
+    '.jpeg': { type: 'image/jpeg' },
+    '.jpg': { type: 'image/jpg' }
+};
 
 //-- Función del servidor
 const server = http.createServer((req, res) => {
@@ -37,26 +45,49 @@ const server = http.createServer((req, res) => {
             }
         });
     
-    //-- Error 404
-        
-    } else {       
-        fs.readFile(error_path, (err, data) => {
-            if (err) { 
-                console.error('Error al leer el archivo:', err);
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                fs.readFile(error_path, (err, data) => {
-                    if (err) {
-                        res.end('Error 404: File not found'); 
-                    } else {
-                        res.end(data);
-                    }
-                });
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
+    //-- Otras opciones incluyendo error 404
+    } else {
+        const ext = path.extname(pathname);
+        const mimeInfo = mimeTypes[ext];
+        if (mimeInfo) { //-- Si etá registrado el tipo de archivo:
+            const recurso = path.join(__dirname, pathname); //-- Obtención de la ruta del recurso
+            fs.readFile(recurso, (err, data) => {
+                if (err) {
+                    console.error('Error al leer el archivo:', err);
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    fs.readFile(error_path, (err, data) => {
+                        if (err) {
+                            res.end('Error 404: File not found'); 
+                        } else {
+                            res.end(data);
+                        }
+                    });
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data); //-- Envío del recurso pedido
+                }
+            })
+        //-- Si no es un tipo de archivo registrado:
+        } else {       
+            fs.readFile(error_path, (err, data) => {
+                if (err) { 
+                    console.error('Error al leer el archivo:', err);
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    fs.readFile(error_path, (err, data) => {
+                        if (err) {
+                            res.end('Error 404: File not found'); 
+                        } else {
+                            res.end(data);
+                        }
+                    });
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
         });
-    }})
+    }}
+});
+
 
 server.listen(PUERTO, function () {
     console.log('Servidor funcionando en el puerto ' + PUERTO);
